cda550eebcde32129593b33742b2eff03ecf6fcc
kingz1127 <osunyingboadedeji1@gmail.com>
2026-07-11 09:19:17 +0100

    frontend context2

--- devops-cloundsandbox-frontend/src/api/client.ts
+++ devops-cloundsandbox-frontend/src/api/client.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8080/api/v1',
+
+  baseURL: window.location.hostname === 'localhost' 
+    ? 'http://localhost:8080/api/v1' 
+    : `http://${window.location.hostname}/api/v1`,
   headers: {
     'Content-Type': 'application/json',
   },
