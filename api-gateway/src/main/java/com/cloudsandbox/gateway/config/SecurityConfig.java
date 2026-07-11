dd07a0dfae72a14a6a706dfced11e7820311e15e
kingz1127 <osunyingboadedeji1@gmail.com>
2026-07-11 18:47:40 +0100

    api gateway

--- api-gateway/src/main/java/com/cloudsandbox/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/cloudsandbox/gateway/config/SecurityConfig.java
@@ -32,22 +32,22 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public CorsWebFilter corsWebFilter() {
-        CorsConfiguration corsConfig = new CorsConfiguration();
-        // Be specific! Do not use "*" if you use AllowCredentials(true)
-        corsConfig.setAllowedOrigins(Arrays.asList(
-                "http://localhost:5173",
-                "http://localhost:3000",
-                "https://dev-ops-cloud-sand-box.vercel.app"));
-        corsConfig.setMaxAge(3600L);
-        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
-        corsConfig.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization", "X-User-Id", "Accept"));
-        corsConfig.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig);
-
-        return new CorsWebFilter(source);
-    }
+//    @Bean
+//    public CorsWebFilter corsWebFilter() {
+//        CorsConfiguration corsConfig = new CorsConfiguration();
+//
+//        corsConfig.setAllowedOrigins(Arrays.asList(
+//                "http://localhost:5173",
+//                "http://localhost:3000",
+//                "https://dev-ops-cloud-sand-box.vercel.app"));
+//        corsConfig.setMaxAge(3600L);
+//        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+//        corsConfig.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization", "X-User-Id", "Accept"));
+//        corsConfig.setAllowCredentials(true);
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", corsConfig);
+//
+//        return new CorsWebFilter(source);
+//    }
 }
--- devops-cloundsandbox-frontend/src/api/client.ts
+++ devops-cloundsandbox-frontend/src/api/client.ts
@@ -9,7 +9,6 @@ const apiClient = axios.create({
   },
 });
 
-// Interceptor to add JWT
 apiClient.interceptors.request.use((config) => {
   const token = localStorage.getItem('accessToken');
   const userId = localStorage.getItem('userId'); 
